2472ca24ceb54d4563e8c48ae0b1feccb0711bee
hansteve86 <hansteven86@yahoo.com>
2022-01-13 16:56:59 -0800

    Update WT_BL_data_process.py

--- WT_BL_data_process.py
+++ WT_BL_data_process.py
@@ -13,23 +13,23 @@ Created on Tue Jan 11 10:35:21 2022
 
 @author: hansteven86
 """
-
+#importing necessary library
 import pandas as pd
 import matplotlib.pyplot as plt
 import statistics as stats
 
 
 
-#####wind tunnel data process with pandas
-
+#imports .csv file with windtunnel data
 df = pd.read_csv('lab1file2.csv')
-#print(df)
 
+#port 1-10 are recorded on the wing, 11-20 are atmospheric pressure
 cols1 = ['P 1','P 2','P 3','P 4','P 5'
         ,'P 6','P 7','P 8','P 9','P 10']
 cols2 = ['P 11','P 12','P 13','P 14','P 15'
         ,'P 16','P 17','P 18','P 19','P 20']
 
+#extract datas recorded in wing ports and take average, then assign to new list
 p_ave_lst_mbar = []
 for items in cols1:
     p = df[items].iloc[1:104]
@@ -39,6 +39,7 @@ for items in cols1:
 
 #print(p_ave_lst_mbar)
 
+#extract datas recorded in atmospheric ports then assign to new list
 ptotal_ave_lst_mbar = []
 for item in cols2:
     ptotal = df[item].iloc[1:104]
@@ -49,12 +50,14 @@ for item in cols2:
 ptotal_mean = stats.mean(ptotal_ave_lst_mbar)
 #print(ptotal_mean)
 
+#find mean of atmospheric ports
 q_quant = df.iloc[1:104,1]
 #print(q_quant)
 q_quant_num = q_quant.apply(pd.to_numeric)
 q_ave = q_quant_num.mean()
 #print(q_ave)
 
+#convert pressure units from mbar to pascal
 q_processed = []
 for values in p_ave_lst_mbar:
     q = abs(values-q_ave-ptotal_mean)
@@ -62,6 +65,7 @@ for values in p_ave_lst_mbar:
     q_processed.append(q_pascal)
 #print(q_processed)
 
+#velocity calculation from dynamic pressure
 rho = 1.19
 v = []
 for value in q_processed:
@@ -70,6 +74,7 @@ for value in q_processed:
 #print(v)
 height = [0.2, 0.16, 0.12, 0.1, 0.08, 0.06, 0.04, 0.03, 0.025, 0.018]
 
+#plots
 plt.figure(1)
 plt.plot(height,v,'o')
 plt.xlabel('port height from plate (m)')
